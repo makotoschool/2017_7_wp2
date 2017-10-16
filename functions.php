d423ae06635a38b6d565afa9eff7b25767115acd
Makoto Imura <jamzip@gmail.com>
2017-10-16 14:58:03 +0900

    first-commit

---
+++ functions.php
@@ -0,0 +1,13 @@
+﻿<?php
+
+//styleを読む
+function theme_scripts( ) {
+	wp_enqueue_style( 'reset-css', get_template_directory_uri( ) . '/css/reset.css' );
+    wp_enqueue_style( 'theme-css', get_template_directory_uri( ) . '/css/main.css');
+
+    }
+
+    add_action( 'wp_enqueue_scripts', 'theme_scripts' );
+
+
+
---
+++ index.php
@@ -0,0 +1,128 @@
+﻿<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>TOP|○○Design</title>
+
+<?php wp_head();?>
+</head>
+<body>
+<div class="container">
+	<header>
+		<a href="">
+			<h1 class="logo">
+				MakotoDesign
+			</h1>
+		</a>
+		<nav class="utility_nav">
+			<ul>
+			<li><a href="">NEWS</a></li>
+			<li><a href="">SITEMAP</a></li>
+			<li><a href="">PRIVACY</a></li>
+			</ul>
+		</nav>
+		<div class="main_header clear">
+			<h2>
+				<span class="invisible">これも検索エンジン対策の見出しです</span>
+			</h2>
+
+		</div>
+	</header>
+	<nav class="main_nav clearfix">
+		<ul>
+		<li><a href="">menu</a></li>
+		<li><a href="">menu</a></li>
+		<li><a href="">menu</a></li>
+		<li><a href="">menu</a></li>
+		<li><a href="">menu</a></li>
+		<li><a href="">menu</a></li>
+		</ul>
+	</nav>
+	<main>
+		<div class="gloval_main clearfix">
+			<section class="contents">
+				<article class="content_sentence">
+					<h2 class="top_title">ようこそMakotoDesignへ</h2>
+					<div class="top_content">
+						<p>
+						<img src="http://placeimg.com/640/300/people">
+							木曾路はすべて山の中である。あるところは岨づたいに行く崖の道であり、あるところは数十間の深さに臨む木曾川の岸であり、あるところは山の尾をめぐる谷の入り口である。一筋の街道はこの深い森林地帯を貫いていた。
+東ざかいの桜沢から、西の十曲峠まで、木曾十一宿はこの街道に添うて、二十二里余にわたる長い谿谷の間に散在していた。
+道路の位置も幾たびか改まったもので、古道はいつのまにか深い山間に埋もれた。名高い桟も、蔦のかずらを頼みにしたような危い場処ではなくなって、徳川時代の末にはすでに渡ることのできる橋であった。
+新規に新規にとできた道はだんだん谷の下の方の位置へと降って来た。道の狭いところには、木を伐って並べ、藤づるでからめ、それで街道の狭いのを補った。
+長い間にこの木曾路に起こって来た変化は、いくらかずつでも嶮岨な山坂の多いところを歩きよくした。そのかわり、大雨ごとにやって来る河水の氾濫が旅行を困<br>木曾路はすべて山の中である。あるところは岨づたいに行く崖の道であり、あるところは数十間の深さに臨む木曾川の岸であり、あるところは山の尾をめぐる谷の入り口である。一筋の街道はこの深い森林地帯を貫いていた。
+東ざかいの桜沢から、西の十曲峠まで、木曾十一宿はこの街道に添うて、二十二里余にわたる長い谿谷の間に散在していた。
+道路の位置も幾たびか改まったもので、古道はいつのまにか深い山間に埋もれた。名高い桟も、蔦のかずらを頼みにしたような危い場処ではなくなって、徳川時代の末にはすでに渡ることのできる橋であった。
+新規に新規にとできた道はだんだん谷の下の方の位置へと降って来た。道の狭いところには、木を伐って並べ、藤づるでからめ、それで街道の狭いのを補った。
+長い間にこの木曾路に起こって来た変化は、いくらかずつでも嶮岨な山坂の多いところを歩きよくした。そのかわり、大雨ごとにやって来る河水の氾濫が旅行を困
+						</p>
+					</div>
+
+
+
+				</article>		
+
+			</section>
+
+			<aside class="sidemenu">
+				<div class="widget">
+					<h3 class="widget_title">タイトル</h3>
+					<div class="widget_content"></div>
+				
+				</div>
+				<div class="widget">
+					<h3 class="widget_title">タイトル</h3>
+					<div class="widget_content"></div>
+				</div>
+				<div class="widget">
+					<h3 class="widget_title">タイトル</h3>
+					<div class="widget_content"></div>
+				</div>
+			</aside>
+
+		</div>
+		<section class="content_fullwidth clearfix">
+			<h2>新着記事</h2>
+			<ul>				
+			<li class="post_prev">
+				<h3 class="post_prev_title">新着ダミータイトル</h3>
+				<div class="post_prev_content"></div>
+			</li>
+			<li class="post_prev">
+				<h3 class="post_prev_title">新着ダミータイトル</h3>
+				<div class="post_prev_content"></div>				
+			</li>
+			<li class="post_prev">
+				<h3 class="post_prev_title">新着ダミータイトル</h3>
+				<div class="post_prev_content"></div>
+			</li>
+			<li class="post_prev">
+				<h3 class="post_prev_title">新着ダミータイトル</h3>
+				<div class="post_prev_content"></div>
+			</li>
+			</ul>
+		</section>
+		<section class="content_fullwidth">
+			<h2>固定記事</h2>
+			<article class="content_sentence_fullwidth">
+				<h3 class="top_satatic_title">ダミータイトル</h3>
+				<div class="top_satatic_content">
+					<p>
+					吾輩は猫である。
+名前はまだ無い。どこで生れたかとんと見当がつかぬ。何でも薄暗いじめじめした所でニャーニャー泣いていた事だけは記憶している。
+吾輩はここで始めて人間というものを見た。しかもあとで聞くとそれは書生という人間中で一番獰悪な種族であったそうだ。この書生というのは時々我々を捕えて煮て食うという話である。しかしその当時は何という考もなかったから別段恐しいとも思わなかった。ただ彼の掌に載せられてスーと持ち上げられた時何だかフワフワした感じがあったばかりである。掌の上で少し落ちついて書生の顔を見たのがいわゆる人間というものの見始であろう。この時妙なものだと思った感じが今でも残っている。第一毛をもって装飾されべきはずの顔がつるつるしてまるで薬缶だ。
+その後猫にもだいぶ逢ったがこんな片輪には一度も出会わした事がない。
+のみならず顔の真中があまりに突起している。そうしてその穴の中から時々ぷうぷうと煙を吹く。どうも咽せぽくて実に弱った。これが人間の飲む煙草というものである事はようやくこの頃知った。吾輩は猫である。名前はまだ無い。どこで生れたかとんと見当がつかぬ。何でも薄暗いじめじめした所でニャーニャー泣いていた事だけは記憶している。
+吾輩はここで始めて人間というものを見た。しかもあとで聞くとそれは書生という人間中で一番獰悪な種族であったそうだ。この書生というのは時々我々を捕えて煮て食うという話である。しかしその当時は何という考もなかったから別段恐しいとも思わなかった。ただ彼の掌に載せられてスーと持ち上げられた時何だかフワフワした感じがあったばかりである。掌の上で少し落ちついて書生の顔を見たのがいわゆる人間というものの見始であろう。この時妙なものだと思った感じが今でも残っている。第一毛をもって装飾されべきはずの顔がつるつるしてまるで薬缶だ。その後猫にもだいぶ逢ったがこんな片輪には一
+					</p>
+
+				</div>
+			</article>			
+		</section>
+	</main>
+	<footer>
+	</footer>
+</div>
+<?php wp_footer();?>
+</body>
+</html>
