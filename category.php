567b71e2d445ea3b1461ab2299b5c4e17a0a2f3e
Makoto Imura <jamzip@gmail.com>
2017-10-20 15:31:22 +0900

    single状態完成

---
+++ category.php
@@ -0,0 +1,22 @@
+﻿<?php get_header();?>
+	<main>
+		<div class="gloval_main clearfix">
+			<section class="contents">
+				
+					<?php if(have_posts()):while(have_posts()):the_post();?>
+						<article class="content_sentence">
+							<h2 class="top_title"><a href="<?php the_permalink();?>"><?php the_title();?></a></h2>
+							<div class="top_content"><?php the_excerpt( );?></div>
+						</article>
+					<?php endwhile;endif;?>
+						
+
+			</section>
+
+			<?php get_sidebar();?>
+
+		</div>
+
+	</main>
+<?php get_footer();?>
+
---
+++ page.php
@@ -0,0 +1,22 @@
+﻿<?php get_header();?>
+	<main>
+		<div class="gloval_main clearfix">
+			<section class="contents">
+				
+					<?php if(have_posts()):while(have_posts()):the_post();?>
+						<article class="content_sentence">
+							<h2 class="top_title"><?php the_title();?></h2>
+							<div class="top_content"><?php the_content();?></div>
+						</article>
+					<?php endwhile;endif;?>
+						
+
+			</section>
+
+			<?php get_sidebar();?>
+
+		</div>
+
+	</main>
+<?php get_footer();?>
+
