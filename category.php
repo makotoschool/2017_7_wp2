480e0748440a465129187a9ccd30207f21e0d3c6
Makoto Imura <jamzip@gmail.com>
2017-10-20 16:03:07 +0900

    single.phpに次の記事へのリンク追加

--- category.php
+++ category.php
@@ -6,7 +6,11 @@
 					<?php if(have_posts()):while(have_posts()):the_post();?>
 						<article class="content_sentence">
 							<h2 class="top_title"><a href="<?php the_permalink();?>"><?php the_title();?></a></h2>
-							<div class="top_content"><?php the_excerpt( );?></div>
+							<div class="top_content">
+								<?php the_excerpt( );?>
+								<a href="<?php the_permalink();?>">続きを読みたい！</a>
+
+							</div>
 						</article>
 					<?php endwhile;endif;?>
 						
--- single.php
+++ single.php
@@ -8,6 +8,11 @@
 							<h2 class="top_title"><?php the_title();?></h2>
 							<div class="top_content"><?php the_content();?></div>
 						</article>
+						<?php
+							previous_post_link('%link', '前の記事', TRUE);
+							next_post_link('%link', '次の記事', TRUE);
+						?>
+
 					<?php endwhile;endif;?>
 						
 
